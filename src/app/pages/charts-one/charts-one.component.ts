c500414ee2ac6dda5aa95d0c1645fbb37926a502
Gerardo Gallegos <puxerop@gmail.com>
2020-02-10 16:30:30 -0600

    Initial commit

---
+++ src/app/pages/charts-one/charts-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-charts-one',
+  templateUrl: './charts-one.component.html',
+  styles: []
+})
+export class ChartsOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
